7124ac1841599854c425af52a30b0d77d63e07ef
Ajanta <36396767+das744@users.noreply.github.com>
2025-01-25 22:43:07 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,8 @@
 const nextConfig = {
     experimental: {
       appDir: true,
-    },
-    env: {
-      WORDPRESS_API_URL: "http://localhost:10008/wp-json",
-    },
+    }
+ 
   };
   
   export default nextConfig;
