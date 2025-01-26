6466392151e429094757aa0feca9255f429d4e01
Ajanta <36396767+das744@users.noreply.github.com>
2025-01-27 09:25:03 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    }
- 
-  };
-  
-  export default nextConfig;
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false, // File system module
+      net: false, // Networking module
+      dns: false, // DNS module
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
   
